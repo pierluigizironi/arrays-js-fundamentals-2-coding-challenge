11270999601bb32bb62dca3d20ce636e62ab929a
pierluigizironi <zironipierluigi@gmail.com>
2022-05-25 22:24:22 +0200

    change name of the second array

--- script.js
+++ script.js
@@ -33,4 +33,4 @@ calcTip(10);
 
 const bills = [125, 555, 44];
 
-const totalTips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
